6617dd6002ea77f6b7932034d4a5410e3954105b
enzo2022 <enzo142014@hotmail.com>
2022-11-07 19:22:06 -0300

    introduje la carpeta client/landingpage/landingpage.jsx

---
+++ client/src/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+
+
+export default function LandingPage(){
+    return(
+        <div >
+           <h1>a trabajar se a dicho</h1> 
+            
+        </div>
+        )
+}
