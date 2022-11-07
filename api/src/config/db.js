65a96c797e8a3290fa10cc3cc1878e562c151455
Chantal <chanticou@gmail.com>
2022-11-07 15:32:08 -0300

    db-connection

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { user, password, host, database } = require("../utils/config.js");
-console.log(user);
+
 const sequelize = new Sequelize(
   `postgres://${user}:${password}@${host}:5432/${database}`,
   {
