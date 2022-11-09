7aa4604b12e4b1f0bdda824d595cb7ff8caa5c27
jhoniernem <jhonny30ca@gmail.com>
2022-11-09 10:25:54 -0500

    Updated files For tailwindcss configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import { store } from "../src/redux/store/index";
 import { Provider } from "react-redux";
