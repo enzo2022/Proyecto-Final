783e3e11c381c6de4e90188683e8f042aad47679
enzo2022 <enzo142014@hotmail.com>
2022-11-10 13:21:11 -0300

    correguir error

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export function getallProperties(){
     return async function(dispatch){
-        const resu = await axios.get('http://localhost:3001/getHouses')
+        const resu = await axios.get('http://localhost:3001/properties/getAll')
         dispatch({
             type:"GET_ALL_PROPERTIES",
             payload: resu.data
