8879e18520f0044977eff130a6fdf497524ffa58
jhoniernem <jhonny30ca@gmail.com>
2022-11-16 19:13:45 -0500

    add app js

--- api/src/app.js
+++ api/src/app.js
@@ -11,27 +11,31 @@ const server = express();
 server.name = "API";
 
 //ROUTES
-app.use("/", require("./routes/properties.routes"));
-app.use("/user", require("./routes/user.routes"));
-app.use("/", require("./routes/properties.routes"));
-app.use("/", require("./routes/cities_routes"));
-app.use("/properties", require("./routes/properties.routes"));
+//Middleware
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
+server.use(morgan("dev"));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
-(async function () {
-  await sequelize.sync({ force: true });
-  app.listen(PORT, async () => {
-    await UpCities();
-    functionJson();
-    console.log("Listening on port ", PORT);
-  });
-})();
+server.use("/", routes); //index
 
-// const express = require("express");
-// const cookieParser = require("cookie-parser");
-// const bodyParser = require("body-parser");
-// const morgan = require("morgan");
-// const routes = require("./routes/index.js");
-// const cors = require("cors");
-// require("./db.js");
+// Error catching endware.
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = server;
