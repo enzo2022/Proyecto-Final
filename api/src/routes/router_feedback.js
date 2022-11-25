d5f3db44bc52ff461efc5b171c7d38fa337aa9e7
musicologo07 <106623570+musicologo07@users.noreply.github.com>
2022-11-25 17:53:09 -0300

    delete feedback

--- api/src/routes/router_feedback.js
+++ api/src/routes/router_feedback.js
@@ -6,6 +6,7 @@ const {
   feedbackById_Property,
   feedbackById_User,
   answerFeedback,
+  deleteFeetback,
   // isBossFeedback,
 } = require("../controllers/controller_feedback");
 
@@ -13,5 +14,6 @@ router.post("/feedback/createFeedback", createFeedback);
 router.get("/feedback/property/:id_Property", feedbackById_Property);
 router.get("/feedback/user/:id_User", feedbackById_User);
 router.post("/feedback/answerFeedback", answerFeedback);
+router.delete("/feedback/deleteFeetback/:id_Feedback", deleteFeetback);
 
 module.exports = router;
