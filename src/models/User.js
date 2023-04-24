d956d1fa6ef0edfbf48cb4979c9cbf39ed431b55
jhoniernem <jhonny30ca@gmail.com>
2023-04-24 10:36:49 -0500

    Refactor : user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,53 +1,26 @@
 const { DataTypes } = require("sequelize");
 
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define(
     "User",
     {
-      id_User: {
+      idUser: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
         primaryKey: true,
       },
-
-      photo: {
+      fName: {
         type: DataTypes.STRING,
-        defaultValue:
-          "https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png",
+        allowNull: false,
+      },
+      lName: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
       userName: {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-
-      user_auth_0: {
-        type: DataTypes.BOOLEAN,
-        allowNull: true,
-        defaultValue: false,
-      },
-      rating: {
-        type: DataTypes.ENUM("1", "2", "3", "4", "5"),
-        defaultValue: "1",
-      },
-
-      user_type: {
-        type: DataTypes.ENUM(
-          "admin",
-          "userPremiun",
-          "userLogged",
-          "userNotLogged"
-        ),
-        defaultValue: "userNotLogged",
-      },
-
       email: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -56,21 +29,42 @@ module.exports = (sequelize) => {
           isEmail: true,
         },
       },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        get() {
+          return this.getDataValue("state") ? "online" : "offline";
+        },
+      },
+      photo: {
+        type: DataTypes.STRING,
+        defaultValue:
+          "https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png",
+      },
+      user_auth_0: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: false,
+      },
+      rating: {
+        type: DataTypes.ENUM("0", "1", "2", "3", "4", "5"),
+        defaultValue: "0",
+      },
+      userType: {
+        type: DataTypes.ENUM("admin", "premium", "logged"),
+        defaultValue: "logged",
+      },
       cellphone: {
         type: DataTypes.BIGINT,
         allowNull: true,
       },
-
       state: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-        get() {
-          if (this.getDataValue("state")) {
-            return "Activado";
-          } else {
-            return "Desactivado";
-          }
-        },
+        type: DataTypes.ENUM("pending", "normal", "blocked"),
+        defaultValue: "pending",
       },
     },
     {
