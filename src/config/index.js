53aa31464c4c22eed3833bbcb5fe4a5ab6a103f2
jhoniernem <jhonny30ca@gmail.com>
2023-05-05 21:13:23 -0500

    Refactor | deploy try 2

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,45 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const { DATABASE, SECRET_KEY } = process.env;
+const { DATABASE, SECRET_KEY, NODE_ENV } = process.env;
 
-let sequelize = new Sequelize(DATABASE, {
-  logging: false,
-  native: false,
-});
+let sequelize;
+
+console.log("§↑↓",NODE_ENV);
+console.log("PROCESS.ENV", process.env)
+
+if (NODE_ENV === "production") {
+
+  const {PGDATABASE, PGHOST, PGPORT, PGUSER, PGPASSWORD} = process.env
+
+  sequelize = new Sequelize({
+    database: PGDATABASE,
+    dialect: "postgres",
+    host: PGHOST,
+    port: PGPORT,
+    username: PGUSER,
+    password: PGPASSWORD,
+    pool: {
+      max: 3,
+      min: 1,
+      idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        // Ref.: https://github.com/brianc/node-postgres/issues/2009
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+    ssl: true,
+  });
+} else {
+  sequelize = new Sequelize(DATABASE, {
+    logging: false,
+    native: false,
+  });
+}
 
 module.exports = {
   sequelize,
