ad2f5a2b8b37ca819c6122cddf7d20d21f43dfad
jhoniernem <jhonny30ca@gmail.com>
2023-05-05 22:01:42 -0500

    Refactor | deploy try 4

--- src/config/index.js
+++ src/config/index.js
@@ -1,40 +1,13 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const { DATABASE, SECRET_KEY, NODE_ENV, PGDATABASE, PGHOST, PGPORT, PGUSER, PGPASSWORD } = process.env;
-
-if (!DATABASE && !PGDATABASE) {
-  throw new Error("No se ha definido una variable de entorno para la base de datos");
-}
-
-if (!SECRET_KEY) {
-  throw new Error("No se ha definido una variable de entorno para la clave secreta");
-}
+const { DATABASE, SECRET_KEY, NODE_ENV } = process.env;
 
 let sequelize;
 
 if (NODE_ENV === "production") {
-  sequelize = new Sequelize({
-    database: PGDATABASE || DATABASE,
-    dialect: "postgres",
-    host: PGHOST,
-    port: PGPORT,
-    username: PGUSER,
-    password: PGPASSWORD,
-    pool: {
-      max: 3,
-      min: 1,
-      idle: 10000,
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
-    },
-    ssl: true,
-  });
+  console.log("Production");
+  sequelize = new Sequelize(DATABASE, { logging: false, native: false });
 } else {
   sequelize = new Sequelize(DATABASE, {
     logging: false,
