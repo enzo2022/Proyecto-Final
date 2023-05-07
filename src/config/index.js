b0593ee7eef48b536bd3d440b7aa12b6556b7396
jhoniernem <jhonny30ca@gmail.com>
2023-05-07 09:51:10 -0500

    refactor : Use variable PORT

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const {LOCAL_DATABASE, DATABASE, SECRET_KEY, NODE_ENV, PORT } = process.env;
+const {PGDATABASE, DATABASE, SECRET_KEY, NODE_ENV, PORT } = process.env;
 
 let sequelize;
 
 if (NODE_ENV === "production") {
   console.log("Production");
-  console.log(PORT);
-  sequelize = new Sequelize(DATABASE, { logging: false, native: false });
+  sequelize = new Sequelize(PGDATABASE, { logging: false, native: false });
 } else {
-  console.log("Desarrollo");
-  sequelize = new Sequelize(LOCAL_DATABASE, {
+  console.log("Development");
+  sequelize = new Sequelize(DATABASE, {
     logging: false,
     native: false,
   });
@@ -20,4 +19,5 @@ if (NODE_ENV === "production") {
 module.exports = {
   sequelize,
   SECRET_KEY,
+  PORT
 };
