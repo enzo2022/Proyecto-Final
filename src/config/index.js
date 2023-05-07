a6441523943b474c7c0969b9b9b420edc90e6160
jhoniernem <jhonny30ca@gmail.com>
2023-05-07 09:34:06 -0500

    add .env PORT

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,17 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const { DATABASE, SECRET_KEY, NODE_ENV } = process.env;
+const {LOCAL_DATABASE, DATABASE, SECRET_KEY, NODE_ENV, PORT } = process.env;
 
 let sequelize;
 
 if (NODE_ENV === "production") {
   console.log("Production");
+  console.log(PORT);
   sequelize = new Sequelize(DATABASE, { logging: false, native: false });
 } else {
-  sequelize = new Sequelize(DATABASE, {
+  console.log("Desarrollo");
+  sequelize = new Sequelize(LOCAL_DATABASE, {
     logging: false,
     native: false,
   });
