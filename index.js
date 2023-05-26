c585dfb4652124aa8961ceb6c16142c3d0a19b13
jhoniernem <jhonny30ca@gmail.com>
2023-05-25 22:47:13 -0500

    re deploy

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { conn } = require("./src/db.js");
 const {addInformation} = require("./src/temp")
 
 const PORT = require("./src/config").PORT || 3001 
-const FORCE = false
+const FORCE = true
 
-conn.sync({ alter: true }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   server.listen(PORT, async () => {
     if (FORCE) await addInformation()
     console.log(`%s listening at port → ${PORT}`);
