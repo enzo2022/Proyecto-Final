de87ddb4c279c676dd19aea7ec8aa9f56628514c
jhoniernem <jhonny30ca@gmail.com>
2023-05-28 09:42:43 -0500

    Add alter option

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { PORT } = require('./src/config/index')
 
 const FORCE = false
 const RunsOn = PORT || 3001
-conn.sync({ alter: true }).then(async () => {
+conn.sync({ alter: !FORCE, force: FORCE }).then(async () => {
   server.listen(RunsOn, async () => {
     if (FORCE) await addInformation()
     console.log(`%s listening at port → ${RunsOn}`)
