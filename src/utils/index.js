1ecaacc5d0bb785db7ec7bc429cfc95a8f07281d
jhoniernem <jhonny30ca@gmail.com>
2023-07-04 10:44:59 -0500

    adding exact version of nodemailer

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,13 @@
 const { hashPassword, verifyPassword } = require('./hashPassword')
 const { generateToken } = require('./jwt')
 const entries = require('./entries')
-
+const { sendMail } = require('./nodemailer')
+const template = require('./nodemailer/templates')
 module.exports = {
   hashPassword,
   verifyPassword,
   generateToken,
   entries,
+  sendMail,
+  template,
 }
---
+++ src/utils/nodemailer/templates.js
@@ -0,0 +1,14 @@
+module.exports = {
+  registerMessage: () => `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style></style>
+    </head>
+    <body>
+    </body>
+  </html>
+  `,
+}
